457abf3fc3fdb4d9a17eac4d888efc4026e3cf88
x713 <unx0rz@gmail.com>
2025-11-30 22:57:35 +0000

    Fix database connection variable

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') # or 'you-will-never-guess'
 
     # Read DB URL
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_NAME') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///app.db'
         
     SQLALCHEMY_TRACK_MODIFICATIONS = False
